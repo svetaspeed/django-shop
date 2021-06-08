0691b73aa016818e9555b53951aedaa85200441f
svetaspeed <svetaspeedster@gmail.com>
2021-06-08 18:40:21 +1000

    lesson 2

--- shop/specs/models.py
+++ shop/specs/models.py
@@ -18,8 +18,27 @@ class CategoryFeatures(models.Model):
 
 
 class FeatureValidator(models.Model):
-
+    """
+    Валидатор значений для конкрейтной характеристики принадлежащей к конкретной категории
+    """
     category = models.ForeignKey('mainapp.Category', verbose_name='Категория', on_delete=models.CASCADE)
     feature_key = models.ForeignKey(CategoryFeatures, verbose_name='Ключ характеристики', on_delete=models.CASCADE)
     valid_feature_value = models.CharField(max_length=100, verbose_name='Влаидное значение')
 
+    def __str__(self):
+        return f"Категория \"{self.category.name}\" | " \
+               f"Характеристика \"{self.feature_key.feature_name}\" | " \
+               f" Валидное значение \"{self.valid_feature_value}\""
+
+class ProductFeatures(models.Model):
+    """
+    Характеристики товара
+    """
+    product = models.ForeignKey('mainapp.Product', verbose_name='Товар', on_delete=models.CASCADE)
+    feature = models.ForeignKey(CategoryFeatures, verbose_name='Характеристика', on_delete=models.CASCADE)
+    value = models.CharField(max_length=255, verbose_name='Значение')
+
+    def __str__(self):
+        return f"Товар - \"{self.product.title} |" \
+               f"Характеристика- \"{self.feature.feature_name} |" \
+               f"Значение - {self.value}"
