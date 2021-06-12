d69d2ae8a9c89025e63a03407bb8a205f7aee1f3
svetaspeed <svetaspeedster@gmail.com>
2021-06-12 17:33:33 +1000

    lesson 3

--- shop/specs/views.py
+++ shop/specs/views.py
@@ -1,3 +1,9 @@
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+
+
+class BaseSpecView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'product_features.html', {})
