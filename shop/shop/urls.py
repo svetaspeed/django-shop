3bf85783ce81a6ad0f5a01be1487aa51e172dabb
svetaspeed <svetaspeedster@gmail.com>
2021-06-12 17:48:01 +1000

    lesson 3

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainapp.urls'))
+    path('', include('mainapp.urls')),
+    path('product_specs/', include('specs.urls'))
 ]
 
 
