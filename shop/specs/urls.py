4e60b886222eaa8c68d192a929d6036702845d3c
svetaspeed <svetaspeedster@gmail.com>
2021-06-17 14:05:00 +1000

    lesson 5

--- shop/specs/urls.py
+++ shop/specs/urls.py
@@ -7,5 +7,5 @@ from .views import (
 
 urlpatterns = [
     path('', BaseSpecView.as_view(), name='base-spec'),
-    path('new_category', CreateNewCategory.as_view(), name='new_category')
+    path('new_category', CreateNewCategory.as_view(), name='new-category')
 ]
