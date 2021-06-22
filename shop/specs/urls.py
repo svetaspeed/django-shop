4e10d07d064d784fc20b67896158fc77959a63af
svetaspeed <svetaspeedster@gmail.com>
2021-06-22 18:56:12 +1000

    lesson 6

--- shop/specs/urls.py
+++ shop/specs/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import (
     BaseSpecView,
-    CreateNewCategory
+    CreateNewCategory,
+    CreateNewFeatures
 )
 
 
 urlpatterns = [
     path('', BaseSpecView.as_view(), name='base-spec'),
-    path('new_category', CreateNewCategory.as_view(), name='new-category')
+    path('new-category', CreateNewCategory.as_view(), name='new-category'),
+    path('new-feature/', CreateNewFeatures.as_view(), name='new-feature')
 ]
--- shop/specs/views.py
+++ shop/specs/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpResponseRedirect, JsonResponse
 
-from .models import CategoryFeature, FeatureValidator, ProductFeatures
+from .models import CategoryFeatures, FeatureValidator, ProductFeatures
 from .forms import NewCategoryFeatureKeyForm, NewCategoryForm
 from mainapp.models import Category, Product
 
@@ -77,3 +77,74 @@ class FeatureChoiceView(View):
             res_string += option.format(value=item.feature_name, option_name=item.feature_name)
         html_select = html_select.format(result=res_string)
         return JsonResponse({"result": html_select, "value": int(request.GET.get('category_id'))})
+
+
+class CreateFeatureView(View):
+
+    def get(self, request, *args, **kwargs):
+        category_id = request.GET.get('category_id')
+        feature_name = request.GET.get('feature_name')
+        value = request.GET.get('feature_value').strip(" ")
+        print(value)
+        category = Category.objects.get(id=int(category_id))
+        feature = CategoryFeature.objects.get(category=category, feature_name=feature_name)
+        existed_object, created = FeatureValidator.objects.get_or_create(
+            category=category,
+            feature_key=feature,
+            valid_feature_value=value
+        )
+        if not created:
+            return JsonResponse({
+                "error": f"Значение '{value}' уже существует"
+            })
+        messages.add_message(
+            request, messages.SUCCESS,
+            f'Значение "{value}" для характеристики'
+            f'"{feature.feature_name}" в категории {category.name} успешно создано'
+        )
+        return JsonResponse({'result': 'ok'})
+
+
+class NewProductFeatureView(View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.all()
+        context = {'categories': categories}
+        return render(request, 'new_product_feature.html', context)
+
+
+class SearchProductAjaxView(View):
+
+    def get(self, request, *args, **kwargs):
+        query = request.GET.get('query')
+        category_id = request.GET.get('category_id')
+        category = Category.objects.get(id=int(category_id))
+        products = list(Product.objects.filter(
+            category=category,
+            title_icontains=query
+        ).values())
+        return JsonResponse({"result": products})
+
+
+class AttachNewFeatureToProduct(View):
+
+    def get(self, request, *args, **kwargs):
+        res_string = ""
+        product = Product.objects.get(id=int(request.GET.get('product_id')))
+        existing_features = list(set([item.feature.feature_name for item in product.features.all()]))
+        print(existing_features)
+        category_features = CategoryFeatures.objects.filter(
+            category=product.category
+        ).exclude(feature_name__in=existing_features)
+        option = '<option value="{value}">{option_name}</option>'
+        html_select = """
+            <select class="form-select" name="product-category-features" id="product-category-features-id" aria-label="Default select example">
+                <option selected>---</option>
+                {result}
+            </select>
+                    """
+        for item in category_features:
+            res_string += option.format(value=item.category.id, option_name=item.feature_name)
+            html_select = html_select.format(result=res_string)
+            return JsonResponse({"features": html_select})
+
